4b232d12d6f0cb5ceb5469b0e59282461dc80591
Axel Östan <axel.ostan@Axels-MacBook-Air.local>
2021-10-12 14:47:04 +0200

    js update 4

--- javascript.js
+++ javascript.js
@@ -1,41 +1,14 @@
-function search (){
-    // get GET /orgs/ntijoh-te4/repos
-}
-
 const url = "https://api.github.com";
-const user = "ntijoh-axel-ostan";
-const token = { method: 'GET', headers: { 'Authorization': 'token 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' } };
-const id = "409177162";
-// class api {
-//     static async user_info() {
-//         const getrequest = await fetch(`${url}/user`);
-//         const fetched = await getrequest.json();
-//         console.log(fetched);
-//         return fetched;
-//     }
 
-//     static async users(){
-//         const getrequest = await fetch(`${url}/user/${user}`);
-//         const fetched = await getrequest.json();
-//         console.log(fetched);
-//         return fetched;
-        
-//     }
-    
-//     static async repositories(){
-//         const getrequest = await fetch(`${url}/user/${user}/repos`);
-//         const fetched = await getrequest.json();
-//         console.log(fetched);
-//         return fetched;
-//     }
-    
-// }
+const user = "ntijoh-axel-ostan";
+// const user = document.querySelector('input').value;
+const token = { method: 'GET', headers: { 'Authorization': 'token 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' } };
+const id = "409177162";
 
 function api() {
-    
     async function user_info(){
-        const getrequest = await fetch(`${url}/user`, token);
-        const fetched = await getrequest.json();
+        let getrequest = await fetch(`${url}/user`, token);
+        let fetched = await getrequest.json();
         console.log(fetched);
         return fetched;
     }
@@ -61,12 +34,18 @@ function api() {
         return fetched;
     }
 
-
+    async function repos_path() {
+        const getrequest = await fetch(`${url}/repositories/${id}/contents`, token);
+        const fetched = await getrequest.json();
+        console.log(fetched);
+        return fetched;
+    }
 
     user_info();
     users();
     repositories();
     repository();
+    repos_path();
 }
 api();
 
