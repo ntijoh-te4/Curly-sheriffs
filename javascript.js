2f8a4c1f610fc650043ca9c9612c424688d48352
melwin-bruun-nti-johanneberg <melwin.bruun@elev.ga.ntig.se>
2021-10-12 11:22:08 +0200

    working  html and js for index page

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
-console.log("hewj")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function index(){
+    const main = document.querySelector('main');
+    const template = document.querySelector('.start_words');
+    const clone = template.content.cloneNode(true);
+
+    main.appendChild(clone)
+}
