9b494c43d01b0e9a26fa8a690740e7903bd7d4f8
Axel Östan <axel.ostan@Axels-MacBook-Air.local>
2021-10-12 12:18:12 +0200

    js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 
+function search (){
+    // get GET /orgs/ntijoh-te4/repos
+}
+
+async function get_rep() {
+    let user = "ntijoh-te4";
+    let rep = "terminalen";
+    let url = "https://api.github.com/search/{user}/{rep}";
+    const response = await fetch(url);
+    const result = await response.json();
+
+    console.log(result);
+}
+
+get_rep();
