1d035d97f796add81b8e225cd798870526e2f0c2
Tranzite <david.roman@elev.ga.ntig.se>
2021-10-13 14:27:57 +0200

    Search function works in console

--- javascript.js
+++ javascript.js
@@ -17,10 +17,12 @@ const id = "409177162";
 
 
 // eventlistener på input
-document.querySelector('#search').addEventListener("focus", api);
+document.querySelector('#search').addEventListener("input", api);
 
 //api funktionen på eventlistener
 function api() {
+    let searchInput = document.querySelector("#search").value
+    console.log(searchInput)
 
     async function user_info(){
         let getrequest = await fetch(`${url}/user`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
@@ -28,28 +30,30 @@ function api() {
         console.log(fetched);
         return fetched;
     }
-
+     // tar in en användare
      async function users() {
-         let getrequest = await fetch(`${url}/users/${user}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
+         let getrequest = await fetch(`${url}/users/${searchInput}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
          let fetched = await getrequest.json();
          console.log(fetched);
+         console.log(searchInput);
          return fetched;
      }   
-
+     // tar in repositories från nämnd användare
      async function repositories() {
-         let getrequest = await fetch(`${url}/users/${user}/repos`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
+         let getrequest = await fetch(`${url}/users/${searchInput}/repos`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
          let fetched = await getrequest.json();
          console.log(fetched);
          return fetched;
      }
-
+     // tar in en specifik repository från användaren
      async function repository() {
          let getrequest = await fetch(`${url}/repositories/${id}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
          let fetched = await getrequest.json();
          console.log(fetched);
+
          return fetched;        
      }
-
+     
      async function repos_path() {
          let getrequest = await fetch(`${url}/repositories/${id}/contents`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
          let fetched = await getrequest.json();
