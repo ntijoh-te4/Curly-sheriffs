e6bd0e70bd19b99ee6a914bfa616eb40149eff55
Axel Östan <axel.ostan@Axels-MacBook-Air.local>
2021-10-14 14:48:34 +0200

    clean up

--- javascript.js
+++ javascript.js
@@ -19,129 +19,45 @@ document.querySelector('form#input').addEventListener("submit", api);
 
 //api funktionen på eventlistener
 function api(e) {
+
     e.preventDefault();
-
     let searchInput = document.querySelector("#search").value
-    console.log(searchInput)
-
-
-     // tar in en användare
-     // async function users() {
-     //    let getrequest = await fetch(`${url}/users/${searchInput}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-      //   let fetched = await getrequest.json();
-    //      let info = JSON.stringify(fetched.login);
-    //      console.log(info);
-       //  console.log(fetched);
-       //  console.log(searchInput);
-      //   return fetched;
-   //  } 
-
-
-    // hämtar username info
-    //  async function users() {
-    //      let getrequest = await fetch(`${url}/users/${user}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-    //      let fetched = await getrequest.json();
-    //      let info = JSON.stringify(fetched.login);
-    //      console.log(info);
-    //      return fetched;
-    //  }   
-
-    //  async function repositories() {
-    //      let getrequest = await fetch(`${url}/users/${searchInput}/repos`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-    //      let fetched = await getrequest.json();
-         
-    //      const container = document.querySelector(".container content");
-    //      const tmpl = document.querySelector("#repo");
-    //         for (let i = 0; i < fetched.length; i++) {
-    //             let name = JSON.stringify(fetched[i].name);
-    //             let forks = JSON.stringify(fetched[i].forks);
-    //             console.log(name);
-    //             console.log(forks);
-    //          }
-    //          console.log(fetched);
-         
-
-    //      return fetched;
-    //  }
-     // tar in en specifik repository från användaren
-    //  async function repository() {
-    //      let getrequest = await fetch(`${url}/repositories/${id}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-    //      let fetched = await getrequest.json();
-    //      console.log(fetched);
-
-    //      return fetched;        
-    //  }
-     
-    //  async function repos_path() {
-    //      let getrequest = await fetch(`${url}/repositories/${id}/contents`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-    //      let fetched = await getrequest.json();
-    //      console.log(fetched);
-    //      return fetched;
-    //  }
-
 
      async function repositories() {
-
         let repos = await fetch(`${url}/users/${searchInput}/repos`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
         let repos_fetched = await repos.json();
 
-
-        //let path_fetched = await getrequest.json();
-        
-    
-        //const card = document.querySelector(".card-content");
-        // let card_name = document.querySelector("#name");
-        // const card_href = document.querySelector("a");
-        // const card_forks = document.querySelector("#forks");
-
-        // let main = document.querySelector("main");
-        // main.innerHTML = "";
-
            for (let i = 0; i < repos_fetched.length; i++) {
                let repo_name = JSON.stringify(repos_fetched[i].name);
-
                let repo_forks = JSON.stringify(repos_fetched[i].forks);
-            //    let id = JSON.stringify(repos_fetched[i].id);
+               let id = JSON.stringify(repos_fetched[i].id);
 
-               console.log(repo_name);
-               
-               
                const container = document.querySelector(".row");
                const tmpl = document.querySelector("#repo");
                const clone = tmpl.content.cloneNode(true);
-
-               let id = JSON.stringify(repos_fetched[i].id);
-               console.log(id);
- 
                const _name = document.createTextNode(repo_name);
-               clone.querySelector("#name").appendChild(_name);
+
                clone.querySelector('#showForks').addEventListener("click", files);
                clone.querySelector('#showForks').id = id;
-            
+
                const addName = document.createTextNode(repo_name);
                const addForks = document.createTextNode(repo_forks);
                clone.querySelector("#name").appendChild(addName);
                clone.querySelector("#forks").appendChild(addForks);
                container.appendChild(clone);
             }
-            console.log(repos_fetched);
         
 
         return repos_fetched;
     }
-    //kör dom olika funktionerna
-    // users();
      repositories();
-    //  repository();
-    //  repos_path();
-}
 
-// ny test constant
+}
 
 
 async function files(event){
-    let forkid = event.target.id;
     
+    let forkid = event.target.id;
     let main = document.querySelector("main");
     main.innerHTML = "";
 
