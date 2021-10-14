ffc01bc37320d2fa7db58f8b0ea1c50594765f04
Axel Östan <axel.ostan@Axels-MacBook-Air.local>
2021-10-14 13:41:07 +0200

    cards

--- javascript.js
+++ javascript.js
@@ -12,13 +12,11 @@ async function getToken() {
 
 // constanter 
 const url = "https://api.github.com";
-const user = "ntijoh-axel-ostan";
-const id = "409177162";
-
 
 
 // eventlistener på input
 document.querySelector('#search').addEventListener("input", api);
+document.querySelector('#fork').addEventListener("click", files);
 
 //api funktionen på eventlistener
 function api() {
@@ -65,27 +63,26 @@ function api() {
     //      return fetched;
     //  }
      // tar in en specifik repository från användaren
-     async function repository() {
-         let getrequest = await fetch(`${url}/repositories/${id}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-         let fetched = await getrequest.json();
-         console.log(fetched);
+    //  async function repository() {
+    //      let getrequest = await fetch(`${url}/repositories/${id}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
+    //      let fetched = await getrequest.json();
+    //      console.log(fetched);
 
-         return fetched;        
-     }
+    //      return fetched;        
+    //  }
      
-     async function repos_path() {
-         let getrequest = await fetch(`${url}/repositories/${id}/contents`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-         let fetched = await getrequest.json();
-         return fetched;
-     }
+    //  async function repos_path() {
+    //      let getrequest = await fetch(`${url}/repositories/${id}/contents`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
+    //      let fetched = await getrequest.json();
+    //      console.log(fetched);
+    //      return fetched;
+    //  }
 
 
      async function repositories() {
         let repos = await fetch(`${url}/users/${searchInput}/repos`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
         let repos_fetched = await repos.json();
-        let path = await fetch(`${url}/repositories/${id}/contents`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
-        let path_fetched = await getrequest.json();
-        
+
         const container = document.querySelector(".container content");
         const tmpl = document.querySelector("#repo");
         const card = document.querySelector(".card-content");
@@ -98,21 +95,48 @@ function api() {
                let name = JSON.stringify(repos_fetched[i].name);
                let forks = JSON.stringify(repos_fetched[i].forks);
                let id = JSON.stringify(repos_fetched[i].id);
+
                console.log(name);
                console.log(forks);
+               console.log(id);
 
             }
-            console.log(fetched);
         
 
-        return fetched;
+        return repos_fetched;
     }
     //kör dom olika funktionerna
     // users();
      repositories();
-     repository();
-     repos_path();
+    //  repository();
+    //  repos_path();
 }
 
+// ny test constant
+const id = "417086855";
+async function files(){
+   
 
+    let path = await fetch(`${url}/repositories/${id}/contents`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
+    let path_fetched = await path.json();
+    console.log(path_fetched);
+    let file = JSON.stringify(path_fetched[1].download_url);
+    let html_link = path_fetched[1].html_url;
+ 
+    console.log(file);
+
+    let main = document.querySelector("main");
+    let filecardtemplate = document.querySelector("#fork");
+    const fork_clone = filecardtemplate.content.cloneNode(true);
+
+    fork_clone.querySelector(".fork-title").textContent = "Api-test";
+    fork_clone.querySelector("code").textContent = file;
+    fork_clone.querySelector("a").href = html_link;
+    fork_clone.querySelector(".unit-tests").textContent= "40p";
+    
+    main.appendChild(fork_clone);
+
+}
+
+files();
 
