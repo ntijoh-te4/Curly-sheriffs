629ff11d521dadb03aa696c236e3afab9dc627e8
Tranzite <david.roman@elev.ga.ntig.se>
2021-10-15 10:17:54 +0200

    manifest test

--- javascript.js
+++ javascript.js
@@ -19,7 +19,7 @@ document.querySelector('form#input').addEventListener("submit", api);
 
 //api funktionen på eventlistener
 function api(e) {
-
+    
     e.preventDefault();
     let searchInput = document.querySelector("#search").value
 
@@ -27,6 +27,8 @@ function api(e) {
         let repos = await fetch(`${url}/users/${searchInput}/repos`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
         let repos_fetched = await repos.json();
 
+        let test = JSON.stringify(repos_fetched);
+        console.log(repos_fetched)
            for (let i = 0; i < repos_fetched.length; i++) {
                let repo_name = JSON.stringify(repos_fetched[i].name);
                repo_name = repo_name.replaceAll('"', '')
@@ -72,11 +74,16 @@ async function files(event){
  
     console.log(file);
 
+    let repos_fetched_name = await fetch(`${url}/repositories/${forkid}`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() } });
+    let repos_fetched_main_name = await repos_fetched_name.json();
     
+    repos_fetched_main_name = JSON.stringify(repos_fetched_main_name.full_name);
+    repos_fetched_main_name = repos_fetched_main_name.replaceAll('"', '')
+
     let filecardtemplate = document.querySelector("#fork");
     const fork_clone = filecardtemplate.content.cloneNode(true);
 
-    fork_clone.querySelector(".fork-title").textContent = "Api-test";
+    fork_clone.querySelector(".fork-title").textContent = repos_fetched_main_name;
     fork_clone.querySelector("code").textContent = file;
     fork_clone.querySelector("a").href = html_link;
     fork_clone.querySelector(".unit-tests").textContent= "40p";
