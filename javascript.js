c3ae023c96b223a5fab098ab4a3352d425921577
melwin-bruun-nti-johanneberg <melwin.bruun@elev.ga.ntig.se>
2021-10-15 12:25:50 +0200

    alomst working content code

--- javascript.js
+++ javascript.js
@@ -78,16 +78,37 @@ async function files(event){
     let file = JSON.stringify(path_fetched[1].download_url);
     let html_link = path_fetched[1].html_url;
  
-    console.log(file);
 
+
+        // hämtar bara ut ifall den har .manifest.json
+        let info = path_fetched.filter(file => file.name === '.manifest.json')[0];
+        // hämtar ut dens content och sedan decypta den så vi får ut information
+        let response = await fetch(info.url, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() }});
+        let json = await response.json();
+        let code = JSON.parse(atob(json.content.replace(/(\r\n|\n|\r)/gm, '')));
+        console.log(code)
+
+        console.log(code.filePath)
+        //  sedan tar den filepath så vi kan hämta den specilea JS coden som vi behöver
+
+        let code_path = await fetch(`${url}/repos/ntijoh/smallest_of_two/contents/${code.filePath}?ref=master`, { method: 'GET', headers: { 'Authorization': 'token ' + await getToken() }});
+        let code_json = await code_path.json();
+        console.log(code_json)
+
+        // hämta content av fillen och skriv in den i våran html
     
+
+
     let filecardtemplate = document.querySelector("#fork");
     const fork_clone = filecardtemplate.content.cloneNode(true);
 
-    fork_clone.querySelector(".fork-title").textContent = "Api-test";
+    fork_clone.querySelector(".fork-title").textContent = "api";
     fork_clone.querySelector("code").textContent = file;
     fork_clone.querySelector("a").href = html_link;
-    fork_clone.querySelector(".unit-tests").textContent= "40p";
+    fork_clone.querySelector(".unit-tests").textContent = `
+    ${code.tests[0].description} 
+    ${code.tests[1].description}  
+    ${code.tests[2].description}`
     
     main.appendChild(fork_clone);
 
