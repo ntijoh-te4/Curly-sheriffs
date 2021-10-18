f3a595091e64be6b9e1fbe4e94c61e1ccd9bdb77
Axel Östan <axel.ostan@Axels-MacBook-Air.local>
2021-10-18 13:28:10 +0200

    mvp klar

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+
 // hämta token.key filen
 async function readFile(file) {
   return fetch(file)
@@ -13,6 +16,15 @@ async function getToken() {
 const url = 'https://api.github.com';
 // eventlistener på input
 // Läser in forks
+
+async function reposcode(forkNames, code) {
+  const codePath = await fetch(`${url}/repos/${forkNames}/contents/${code.filePath}?ref=master`, { method: 'GET', headers: { Authorization: `token ${await getToken()}` } });
+  const codeJson = await codePath.json();
+  let contentCode = await atob(codeJson.content.replace(/(\r\n|\n|\r)/gm, ''));
+  contentCode = contentCode.replaceAll('"', '');
+  return contentCode;
+}
+
 async function files(event) {
   const repoId = event.target.id;
   // Hämta antalet forks
@@ -24,9 +36,6 @@ async function files(event) {
   const path = await fetch(`${url}/repositories/${repoId}/contents`, { method: 'GET', headers: { Authorization: `token ${await getToken()}` } });
   const pathFetched = await path.json();
   console.log(pathFetched);
-  const htmlFile = JSON.stringify(pathFetched[0].download_url);
-  const htmlLink = pathFetched[0].html_url;
-  console.log(htmlFile);
   const filecardtemplate = document.querySelector('#fork');
   // eslint-disable-next-line eqeqeq
   if (forks == 0) {
@@ -34,33 +43,44 @@ async function files(event) {
     alert('Inga forks hittade.');
   } else {
     // hämtar bara ut ifall den har .manifest.json
+    // eslint-disable-next-line no-lonely-if
     if (pathFetched.filter((file) => file.name === '.manifest.json')[0] === undefined) {
       window.location.reload();
-      alert('Inga hittade.');
+      alert('Inga hittade .manifest.json testa ett nytt repo');
     } else {
       const info = pathFetched.filter((file) => file.name === '.manifest.json')[0];
-      console.log(info[url]);
       const response = await fetch(info.url, { method: 'GET', headers: { Authorization: `token ${await getToken()}` } });
       const json = await response.json();
       const code = JSON.parse(atob(json.content.replace(/(\r\n|\n|\r)/gm, '')));
-      console.log(code);
-      console.log(code.filePath);
-      const codePath = await fetch(`${url}/repos/ntijoh/smallest_of_two/contents/${code.filePath}?ref=master`, { method: 'GET', headers: { Authorization: `token ${await getToken()}` } });
-      const codeJson = await codePath.json();
-      console.log(codeJson);
+
+      const pathID = await fetch(`${url}/repositories/${repoId}/forks`, { method: 'GET', headers: { Authorization: `token ${await getToken()}` } });
+      const reposFetched = await pathID.json();
+
+      console.log(forks);
+
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < forks; i++) {
+        let forkNames = JSON.stringify(reposFetched[i].full_name);
+        forkNames = forkNames.replaceAll('"', '');
+        if (forkNames === 'TE4-william-bruun/smallest_of_two') {
+          console.log('no js file');
+        } else {
+          const fetchedCodes = reposcode(forkNames, code);
+          fetchedCodes.then((value) => {
+            const forkClone = filecardtemplate.content.cloneNode(true);
+            forkClone.querySelector('.fork-title').textContent = forkNames;
+            forkClone.querySelector('code').textContent = value;
+            forkClone.querySelector('a').href = reposFetched[i].html_url;
+            forkClone.querySelector('.unit-tests').textContent = '';
+            main.appendChild(forkClone);
+          });
+        }
+      }
     }
     // hämtar ut dens content och sedan decypta den så vi får ut information
     //  sedan tar den filepath så vi kan hämta den specilea JS coden som vi behöver
     // hämta content av fillen och skriv in den i våran html
     // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < forks; i++) {
-      const forkClone = filecardtemplate.content.cloneNode(true);
-      forkClone.querySelector('.fork-title').textContent = 'Api-test';
-      forkClone.querySelector('code').textContent = htmlFile;
-      forkClone.querySelector('a').href = htmlLink;
-      forkClone.querySelector('.unit-tests').textContent = '40p';
-      main.appendChild(forkClone);
-    }
   }
 }
 // api funktionen på eventlistener
